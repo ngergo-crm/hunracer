a4d7377afe70e5578420c5987d5a5189c6295e6d
gergo <ngergo.crm@gmail.com>
2020-12-01 22:07:28 +0100

    2020 12 01 22:07

--- migrations/Version20201201183158.php
+++ migrations/Version20201201183158.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Auto-generated Migration: Please modify to your needs!!
  */
 final class Version20201201183158 extends AbstractMigration
 {
